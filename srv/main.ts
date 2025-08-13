e9ad65c500c3134b455dc36c19cd522ac7fd9810
Caio Melo <202301172144@alunos.estacio.br>
2025-08-13 22:05:15 +0000

    chore: apply cdstyper in main and update tsconfig paths

--- srv/main.ts
+++ srv/main.ts
@@ -1,3 +1,14 @@
+import { Customer, Customers } from '@models/sales'
+
+const customer: Customer = {
+    email: "teste@teste.com",
+    firstName: "Caio",
+    lastName: "Melo",
+    id: "1234"
+}
+
+const customers: Customers = [customer]
+
 function teste (value: string) {
     console.log(value)
 }
