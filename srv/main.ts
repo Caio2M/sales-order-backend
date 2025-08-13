b45dee65f6874fd01c3c74d120977bcd4474c8b2
Caio Melo <202301172144@alunos.estacio.br>
2025-08-13 23:29:15 +0000

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
 import cds, { Service, Request } from '@sap/cds'
-import { Customers } from '@models/sales'
+import { Customers, Products, SalesOrderItem, SalesOrderItems } from '@models/sales'
 
 export default (service: Service) => {
     service.after('READ', 'Customers', (results: Customers) => {
@@ -12,6 +12,7 @@ export default (service: Service) => {
     
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
         const params = request.data;
+        const items: SalesOrderItems = params.items;
         if (!params.customer_id) {
             return request.reject(400, 'Invalid customer')
         }
@@ -23,6 +24,17 @@ export default (service: Service) => {
         if (!customer) {
             return request.reject(404, 'Customer not found')
         }
-
+        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+        const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
+        const products: Products = await cds.run(productsQuery);
+        for (const item of items) {
+            const dbProduct = products.find(product => product.id === item.product_id)
+            if (!dbProduct) {
+                return request.reject(404, `Product ${item.product_id} was not found`);
+            }
+            if (dbProduct.stock === 0) {
+                return request.reject(400, `Product ${dbProduct.name}(${dbProduct.id}) has no stock`);
+            }
+        }
     })
 }
